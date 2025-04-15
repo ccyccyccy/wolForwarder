59f305d5975e41a17d84a68dfd3d7dcf40fa042b
Chee Yuan <chongyuan1997@gmail.com>
2025-04-15 23:08:15 +0800

    add: script to start

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Path to your virtual environment
+VENV_PATH="./venv"
+
+# Path to your Python script
+SCRIPT_PATH="./wolForwarder.py"
+
+# Activate the virtual environment and run the script in the background using nohup
+nohup "$VENV_PATH/bin/python" "$SCRIPT_PATH" > output.log 2>&1 &
+
+# Print the process ID (optional)
+echo "Python script is running in the background with PID $!"
